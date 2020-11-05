336bcf8e82c74d092fb59412a6769d23b3d4fdd3
Mahmoud Darwish <imahmwd14@gmail.com>
2020-11-05 07:04:29 +0700

    Stage 1

--- app/src/main/java/com/example/databindingexample/MainActivity.kt
+++ app/src/main/java/com/example/databindingexample/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.databindingexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import com.example.databindingexample.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    val function: (v: View, keyCode: Int, event: KeyEvent) -> Boolean = { v, keyCode, event ->
+        binding.invalidateAll()
+        false
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.name = binding.editTextTextPersonName
+
+        binding.editTextTextPersonName.setOnKeyListener(function)
     }
 }
